81938353537e78984e8142f57ccc0fc02dc1566c
Nozanin <ocilovanozanin559@gmail.com>
2026-09-16 15:17:47 +0300

    Task1: статистика случайного массива

--- Task1.Statistics/Program.cs
+++ Task1.Statistics/Program.cs
@@ -1,11 +1,30 @@
 using System;
-namespace Task1
+using System.Linq;
+
+
+Random random = new Random();
+int[] numbers = new int[10];
+for (int i = 0; i < numbers.Length; i++)
 {
-   class Program
-   {
-     static void Main()
-     {
-       Console.WriteLine("Hello, World!");
-     }
-   }
+    numbers[i] = random.Next(1, 101); 
 }
+
+Console.WriteLine($"Массив: {string.Join(", ", numbers)}");
+
+int sum = numbers.Sum();
+
+double product = 1;
+foreach (int num in numbers)
+{
+    product *= num;
+}
+
+int evenCount = numbers.Count(n => n % 2 == 0);
+
+double average = numbers.Average();
+int countAboveAverage = numbers.Count(n => n > average);
+
+Console.WriteLine($"Сумма: {sum}");
+Console.WriteLine($"Произведение: {product:1.23E+00}"); 
+Console.WriteLine($"Чётных чисел: {evenCount}");
+Console.WriteLine($"Больше среднего ({average:F1}): {countAboveAverage}");
